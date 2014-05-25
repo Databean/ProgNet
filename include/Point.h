f75256eae3a6f3bfadcba51c7b5dcec77f979b9e
A. Cody Schuffelen <astronomix@gmail.com>
2014-05-25 01:05:09 -0500

    Switched back to tabs

--- include/Point.h
+++ include/Point.h
@@ -10,147 +10,147 @@
 template<unsigned int dim, class CoordType>
 class Point {
 private:
-    CoordType coords[dim];
+	CoordType coords[dim];
 public:
-    typedef Point<dim, CoordType> Type;
-    
-    Point() {
-        for(unsigned int i = 0; i < dim; i++) {
-            coords[i] = 0;
-        }
-    }
-    Point(const std::initializer_list<CoordType>& init) {
-        if(init.size() == dim) {
-            int index = 0;
-            for(auto it = init.begin(); it != init.end(); it++) {
-                coords[index] = *it;
-                index++;
-            }
-        } else {
-            throw std::runtime_error("Given initializer list has length " + toStr(init.size()) + " but this is a point of dimension " + toStr(dim));
-        }
-    }
-    Point(std::istream& in) {
-        in >> (*this);
-    }
-    Point(const Type& other) {
-        (*this) = other;
-    }
-    ~Point() {
-        
-    }
-    inline const Type& operator=(const Type& other) {
-        for(unsigned int i = 0; i < dim; i++) {
-            coords[i] = other[i];
-        }
-        return *this;
-    }
-    
-    inline CoordType& operator[](const unsigned int& i) {
-        if(i < dim) {
-            return coords[i];
-        } else {
-            throw std::runtime_error("Given index " + toStr(i) + " is less than the dimension " + toStr(dim));
-        }
-    }
-    
-    inline const CoordType& operator[](const unsigned int& i) const {
-        if(i < dim) {
-            return coords[i];
-        } else {
-            throw std::runtime_error("Given index " + toStr(i) + " is less than the dimension " + toStr(dim));
-        }
-    }
-    
-    inline Type operator+(const Type& other) const {
-        Type ret;
-        for(unsigned int i = 0; i < dim; i++) {
-            ret[i] = coords[i] + other[i];
-        }
-        return ret;
-    }
-    
-    inline Type operator+() const {
-        return Type(*this);
-    }
-    
-    inline const Type& operator+=(const Type& other) {
-        for(unsigned int i = 0; i < dim; i++) {
-            coords[i] += other[i];
-        }
-        return *this;
-    }
-    
-    inline Type operator-(const Type& other) const {
-        Type ret;
-        for(unsigned int i = 0; i < dim; i++) {
-            ret[i] = coords[i] - other[i];
-        }
-        return ret;
-    }
-    
-    inline Type operator-() const {
-        Type ret;
-        for(unsigned int i = 0; i < dim; i++) {
-            ret[i] = -(coords[i]);
-        }
-        return ret;
-    }
-    
-    inline const Type& operator-=(const Type& other) {
-        for(unsigned int i = 0; i < dim; i++) {
-            coords[i] -= other[i];
-        }
-        return *this;
-    }
-    
-    inline Type operator*(const CoordType& mod) const {
-        Type ret;
-        for(unsigned int i = 0; i < dim; i++) {
-            ret[i] = coords[i] * mod;
-        }
-        return ret;
-    }
-    
-    inline const Type& operator*=(const CoordType& mod) {
-        for(unsigned int i = 0; i < dim; i++) {
-            coords[i] *= mod;;
-        }
-        return *this;
-    }
-    
-    inline Type operator/(const CoordType& mod) const {
-        Type ret;
-        for(unsigned int i = 0; i < dim; i++) {
-            ret[i] = coords[i] / mod;
-        }
-        return ret;
-    }
-    
-    inline const Type& operator/=(const CoordType& mod) {
-        for(unsigned int i = 0; i < dim; i++) {
-            coords[i] /= mod;;
-        }
-        return *this;
-    }
-    
-    inline bool operator==(const Type& other) {
-        for(unsigned int i = 0; i < dim; i++) {
-            if(coords[i] != other[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    inline bool operator!=(const Type& other) {
-        for(unsigned int i = 0; i < dim; i++) {
-            if(coords[i] != other[i]) {
-                return true;
-            }
-        }
-        return false;
-    }
+	typedef Point<dim, CoordType> Type;
+	
+	Point() {
+		for(unsigned int i = 0; i < dim; i++) {
+			coords[i] = 0;
+		}
+	}
+	Point(const std::initializer_list<CoordType>& init) {
+		if(init.size() == dim) {
+			int index = 0;
+			for(auto it = init.begin(); it != init.end(); it++) {
+				coords[index] = *it;
+				index++;
+			}
+		} else {
+			throw std::runtime_error("Given initializer list has length " + toStr(init.size()) + " but this is a point of dimension " + toStr(dim));
+		}
+	}
+	Point(std::istream& in) {
+		in >> (*this);
+	}
+	Point(const Type& other) {
+		(*this) = other;
+	}
+	~Point() {
+		
+	}
+	inline const Type& operator=(const Type& other) {
+		for(unsigned int i = 0; i < dim; i++) {
+			coords[i] = other[i];
+		}
+		return *this;
+	}
+	
+	inline CoordType& operator[](const unsigned int& i) {
+		if(i < dim) {
+			return coords[i];
+		} else {
+			throw std::runtime_error("Given index " + toStr(i) + " is less than the dimension " + toStr(dim));
+		}
+	}
+	
+	inline const CoordType& operator[](const unsigned int& i) const {
+		if(i < dim) {
+			return coords[i];
+		} else {
+			throw std::runtime_error("Given index " + toStr(i) + " is less than the dimension " + toStr(dim));
+		}
+	}
+	
+	inline Type operator+(const Type& other) const {
+		Type ret;
+		for(unsigned int i = 0; i < dim; i++) {
+			ret[i] = coords[i] + other[i];
+		}
+		return ret;
+	}
+	
+	inline Type operator+() const {
+		return Type(*this);
+	}
+	
+	inline const Type& operator+=(const Type& other) {
+		for(unsigned int i = 0; i < dim; i++) {
+			coords[i] += other[i];
+		}
+		return *this;
+	}
+	
+	inline Type operator-(const Type& other) const {
+		Type ret;
+		for(unsigned int i = 0; i < dim; i++) {
+			ret[i] = coords[i] - other[i];
+		}
+		return ret;
+	}
+	
+	inline Type operator-() const {
+		Type ret;
+		for(unsigned int i = 0; i < dim; i++) {
+			ret[i] = -(coords[i]);
+		}
+		return ret;
+	}
+	
+	inline const Type& operator-=(const Type& other) {
+		for(unsigned int i = 0; i < dim; i++) {
+			coords[i] -= other[i];
+		}
+		return *this;
+	}
+	
+	inline Type operator*(const CoordType& mod) const {
+		Type ret;
+		for(unsigned int i = 0; i < dim; i++) {
+			ret[i] = coords[i] * mod;
+		}
+		return ret;
+	}
+	
+	inline const Type& operator*=(const CoordType& mod) {
+		for(unsigned int i = 0; i < dim; i++) {
+			coords[i] *= mod;;
+		}
+		return *this;
+	}
+	
+	inline Type operator/(const CoordType& mod) const {
+		Type ret;
+		for(unsigned int i = 0; i < dim; i++) {
+			ret[i] = coords[i] / mod;
+		}
+		return ret;
+	}
+	
+	inline const Type& operator/=(const CoordType& mod) {
+		for(unsigned int i = 0; i < dim; i++) {
+			coords[i] /= mod;;
+		}
+		return *this;
+	}
+	
+	inline bool operator==(const Type& other) {
+		for(unsigned int i = 0; i < dim; i++) {
+			if(coords[i] != other[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	inline bool operator!=(const Type& other) {
+		for(unsigned int i = 0; i < dim; i++) {
+			if(coords[i] != other[i]) {
+				return true;
+			}
+		}
+		return false;
+	}
 };
 
 /**
@@ -158,10 +158,10 @@ public:
  */
 template<unsigned int dim, class CoordType>
 inline std::ostream& operator<<(std::ostream& strm, const Point<dim, CoordType>& pt) {
-    for(unsigned int i = 0; i < dim - 1; i++) {
-        strm << pt[i] << " ";
-    }
-    return strm << pt[dim-1];
+	for(unsigned int i = 0; i < dim - 1; i++) {
+		strm << pt[i] << " ";
+	}
+	return strm << pt[dim-1];
 }
 
 /**
@@ -169,10 +169,10 @@ inline std::ostream& operator<<(std::ostream& strm, const Point<dim, CoordType>&
  */
 template<unsigned int dim, class CoordType>
 inline std::istream& operator>>(std::istream& in, Point<dim, CoordType>& pt) {
-    for(unsigned int i = 0; i < dim; i++) {
-        in >> pt[i];
-    }
-    return in;
+	for(unsigned int i = 0; i < dim; i++) {
+		in >> pt[i];
+	}
+	return in;
 }
 
 /**
--- include/Shape.h
+++ include/Shape.h
@@ -22,89 +22,89 @@ bool validPointsInType(const ShapeType& shape, const unsigned int& numPoints);
 template<unsigned int dim, class CoordType>
 class Shape {
 private:
-    std::vector<Point<dim, CoordType>> points;
-    Point<dim, CoordType> off;
-    float rot;
-    Color col;
-    ShapeType ty;
-    
-    template<class A, unsigned int B>
-    friend std::istream& operator>>(std::istream&, Shape&);
+	std::vector<Point<dim, CoordType>> points;
+	Point<dim, CoordType> off;
+	float rot;
+	Color col;
+	ShapeType ty;
+	
+	template<class A, unsigned int B>
+	friend std::istream& operator>>(std::istream&, Shape&);
 public:
-    typedef Shape<dim, CoordType> Type;
-    typedef Point<dim, CoordType> PointType;
-    
-    inline Shape() : points(), off(), rot(0.f), ty(SHAPE_NONE) {
-        
-    }
-    
-    inline Shape(const ShapeType& type, const std::vector<PointType>& points) : points(points), off(), rot(0.f), ty(type) {
-        if(!validPointsInType(type, points.size())) {
-            throw std::runtime_error("Invalid number of points in " + shapeName(type) + " shape: " + toStr(points.size()));
-        }
-    }
-    
-    inline Shape(const Type& other) : points(other.points), off(other.off), rot(other.rot), col(other.col), ty(other.ty) {
-        
-    }
-    
-    inline ~Shape() {
-        
-    }
-    
-    inline const Type& operator=(const Type& other) {
-        points = other.points;
-        ty = other.ty;
-        return *this;
-    }
-    
-    inline PointType& operator[](const unsigned int& index) {
-        if(index < points.size()) {
-            return points[index];
-        } else {
-            throw std::runtime_error("Invalid point access in " + shapeName(ty) + ": tried to get point " + toStr(index) + " but there are only " + toStr(points.size()) + " points.");
-        }
-    }
-    
-    inline const PointType& operator[](const unsigned int& index) const {
-        if(index < points.size()) {
-            return points[index];
-        } else {
-            throw std::runtime_error("Invalid point access in " + shapeName(ty) + ": tried to get point " + toStr(index) + " but there are only " + toStr(points.size()) + " points.");
-        }
-    }
-    
-    inline unsigned int numPoints() const {
-        return points.size();
-    }
-    
-    inline PointType& offset() {
-        return off;
-    }
-    
-    inline const PointType& offset() const {
-        return off;
-    }
-    
-    inline float& rotation() {
-        return rot;
-    }
-    
-    inline const float& rotation() const {
-        return rot;
-    }
-    
-    inline Color& color() {
-        return col;
-    }
-    
-    inline const Color& color() const {
-        return col;
-    }
-    
-    inline const ShapeType& type() const {
-        return ty;
-    }
+	typedef Shape<dim, CoordType> Type;
+	typedef Point<dim, CoordType> PointType;
+	
+	inline Shape() : points(), off(), rot(0.f), ty(SHAPE_NONE) {
+		
+	}
+	
+	inline Shape(const ShapeType& type, const std::vector<PointType>& points) : points(points), off(), rot(0.f), ty(type) {
+		if(!validPointsInType(type, points.size())) {
+			throw std::runtime_error("Invalid number of points in " + shapeName(type) + " shape: " + toStr(points.size()));
+		}
+	}
+	
+	inline Shape(const Type& other) : points(other.points), off(other.off), rot(other.rot), col(other.col), ty(other.ty) {
+		
+	}
+	
+	inline ~Shape() {
+		
+	}
+	
+	inline const Type& operator=(const Type& other) {
+		points = other.points;
+		ty = other.ty;
+		return *this;
+	}
+	
+	inline PointType& operator[](const unsigned int& index) {
+		if(index < points.size()) {
+			return points[index];
+		} else {
+			throw std::runtime_error("Invalid point access in " + shapeName(ty) + ": tried to get point " + toStr(index) + " but there are only " + toStr(points.size()) + " points.");
+		}
+	}
+	
+	inline const PointType& operator[](const unsigned int& index) const {
+		if(index < points.size()) {
+			return points[index];
+		} else {
+			throw std::runtime_error("Invalid point access in " + shapeName(ty) + ": tried to get point " + toStr(index) + " but there are only " + toStr(points.size()) + " points.");
+		}
+	}
+	
+	inline unsigned int numPoints() const {
+		return points.size();
+	}
+	
+	inline PointType& offset() {
+		return off;
+	}
+	
+	inline const PointType& offset() const {
+		return off;
+	}
+	
+	inline float& rotation() {
+		return rot;
+	}
+	
+	inline const float& rotation() const {
+		return rot;
+	}
+	
+	inline Color& color() {
+		return col;
+	}
+	
+	inline const Color& color() const {
+		return col;
+	}
+	
+	inline const ShapeType& type() const {
+		return ty;
+	}
 };
 
 /**
@@ -119,11 +119,11 @@ typedef Shape<2, int> Shape2i;
  */
 template<unsigned int dim, class CoordType>
 std::ostream& operator<<(std::ostream& out, const Shape<dim, CoordType>& shape) {
-    out << shape.numPoints() << " ";
-    for(unsigned int i = 0; i < shape.numPoints(); i++) {
-        out << shape[i] << " ";
-    }
-    return out << shape.type();
+	out << shape.numPoints() << " ";
+	for(unsigned int i = 0; i < shape.numPoints(); i++) {
+		out << shape[i] << " ";
+	}
+	return out << shape.type();
 }
 
 /**
@@ -131,14 +131,14 @@ std::ostream& operator<<(std::ostream& out, const Shape<dim, CoordType>& shape) 
  */
 template<unsigned int dim, class CoordType>
 std::istream& operator>>(std::istream& in, const Shape<dim, CoordType>& shape) {
-    unsigned int numPoints;
-    in >> numPoints;
-    shape.points.clear();
-    for(unsigned int i = 0; i < numPoints; i++) {
-        shape.points.push_back(Shape<dim, CoordType>());
-        in >> shape[i];
-    }
-    return in >> shape.ty;
+	unsigned int numPoints;
+	in >> numPoints;
+	shape.points.clear();
+	for(unsigned int i = 0; i < numPoints; i++) {
+		shape.points.push_back(Shape<dim, CoordType>());
+		in >> shape[i];
+	}
+	return in >> shape.ty;
 }
 
 #endif
